b2e499eb24e6526edd1bdbdb7ee74ad76d1a25cf
Iryna Rudzko <irynarudzko@MacBook-Air-Iryna.local>
2021-03-15 13:01:23 +0300

    feat: add partial solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    // let words = [];
+
+    const lessThanTwenty = [
+        'zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+        'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number === 0) {
+        return 'zero';
+    };
+
+    if (number < 20) {
+        return lessThanTwenty[number];
+    };
+
+    let digit = number % 10;
+
+    if (number < 100) {
+        return tens[Math.floor(number / 10)] + (digit ? " " + lessThanTwenty[digit] : "");
+    }
 }
+
